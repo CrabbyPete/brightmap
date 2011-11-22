ee853b66a3eb3d76d06c582d9151a263b1ad40cb
Pete Douma <pete.douma@gmail.com>
2011-11-22 15:36:50 -0500

    Add flavicon

--- apps/cron/accounting.py
+++ apps/cron/accounting.py
@@ -19,15 +19,19 @@ def connections_for( user, first_day, last_day ):
 def send_email(user, invoice, details = None):
     return
 
-def main():
+def main(month = None):
     """
     Main program to do accounting for all lead buyers and organizers
     """
-    # Bill for last month
     first_day = date.today().replace(day = 1)
-    first_day = first_day.replace( month = first_day.month - 1 )
+    if  month:
+        first_day = first_day.replace( month = int(month) )
+    else:
+        # Bill for last month
+        first_day = first_day.replace( month = first_day.month - 1 )
+    
     last_day  = first_day.replace (month = first_day.month + 1 ) - timedelta( days = 1 )
-
+  
     print "Invoicing for the month of: " + first_day.strftime("%B %Y")
     
     # Initialize the API class
@@ -62,13 +66,25 @@ def main():
            
             
             if total > 0: 
-                # Create an invoice 
-                invoice = Invoice( user = profile.user, 
-                                   title = first_day.strftime("%B %Y"),
-                                   cost = total, 
-                                   first_day = first_day, 
-                                   last_day = last_day 
-                                 )
+                title = first_day.strftime("%B %Y")
+                
+                # See if there is an existing invoice for this month
+                try:
+                    invoice = Invoice.objects.get( user      = profile.user,
+                                                   title     = title,
+                                                   first_day = first_day,
+                                                   last_day  = last_day  )
+                except Invoice.DoesNotExist:
+                    # Create an invoice 
+                    invoice = Invoice( user      = profile.user, 
+                                       title     = title,
+                                       cost      = total, 
+                                       first_day = first_day, 
+                                       last_day  = last_day 
+                                     )
+                else:
+                    invoice.cost = total
+                
                 invoice.save()
             
                 # Try and bill to the credit card
@@ -97,5 +113,31 @@ def main():
                     invoice.status = 'rejected'
                 invoice.save()
                 
+
+import optparse
 if __name__ == '__main__':
-    main()
+    op = optparse.OptionParser( usage="usage: %prog " +" [options]" )
+    # Add options for debugging
+    op.add_option('-d', action="store_true", help = 'Debug no emails sent')
+    op.add_option('-p', action="store_true", help = 'Prompt to send')
+    op.add_option('-m', action="store_true", help = 'Month number to start')
+
+    opts,args = op.parse_args()
+
+    # Check if options were set
+    if opts.d:
+        DEBUG = False
+    else:
+        DEBUG = True
+
+    if opts.p:
+        PROMPT = True
+    else:
+        PROMPT = False
+    
+    if opts.m:
+        month = args[0]
+    else:
+        month = None
+    
+    main( month )
